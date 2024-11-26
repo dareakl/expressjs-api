9c279673e5fe6f9a32239ab19a25601770cd6667
Sathish Raja <msathis.akl@hotmail.com>
2024-11-26 15:46:57 +0800

    query params passed in api

--- src/index.js
+++ src/index.js
@@ -4,18 +4,27 @@ const app = express();
 
 const PORT = process.env.PORT || 5003;
 
+const mockUsers = [
+  { id: 1, username: "sathish", displayName: "Sathish" },
+  { id: 2, username: "raja", displayName: "Raja" },
+  { id: 3, username: "david", displayName: "David" },
+];
 app.get("/", (req, res) => {
   res.send("Hello Express");
 });
 
 app.get("/api/users", (req, res) => {
-  res.send([
-    { id: 1, username: "sathish", displayName: "Sathish" },
-    { id: 2, username: "raja", displayName: "Raja" },
-    { id: 3, username: "david", displayName: "David" },
-  ]);
+  res.send(mockUsers);
 });
 
+app.get("/api/users/:id", (req, res) => {
+  const parseId = parseInt(req.params.id);
+  if (isNaN(parseId))
+    return res.status(400).send({ msg: "Bad Request.Invalid ID" });
+  const findUser = mockUsers.find((user) => user.id === parseId);
+  if (!findUser) return res.sendStatus(404);
+  return res.send(findUser);
+});
 app.get("/api/products", (req, res) => {
   res.send([{ id: 1, name: "chicken", price: "10.00" }]);
 });
