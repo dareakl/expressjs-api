b42692d47b4a4b2ff0cf58e07f537748abced1ec
Sathish Raja <msathis.akl@hotmail.com>
2024-11-26 16:24:10 +0800

    POST request API created sending data

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = process.env.PORT || 5003;
 
 const mockUsers = [
@@ -26,6 +28,14 @@ app.get("/api/users", (req, res) => {
   if (filter && value)
     return res.send(mockUsers.filter((user) => user[filter].includes(value)));
   return res.send(mockUsers);
+});
+
+app.post("/api/users", (req, res) => {
+  console.log(req.body);
+  const { body } = req;
+  const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...body };
+  mockUsers.push(newUser);
+  return res.status(201).send(newUser);
 });
 
 app.get("/api/users/:id", (req, res) => {
