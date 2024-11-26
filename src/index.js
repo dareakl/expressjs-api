b5f035049d4844dcaa957541ea4be29ade594c55
Sathish Raja <msathis.akl@hotmail.com>
2024-11-26 14:15:14 +0800

    simple get routes added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ const app = express();
 
 const PORT = process.env.PORT || 5003;
 
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+
+app.get("/api/users", (req, res) => {
+  res.send([
+    { id: 1, username: "sathish", displayName: "Sathish" },
+    { id: 2, username: "raja", displayName: "Raja" },
+    { id: 3, username: "david", displayName: "David" },
+  ]);
+});
+
+app.get("/api/products", (req, res) => {
+  res.send([{ id: 1, name: "chicken", price: "10.00" }]);
+});
 app.listen(PORT, () => {
-  console.log(`Server rinning on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
