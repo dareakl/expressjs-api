1dbe042ca31cbf7f1b469523bbd3acef573349c8
Sathish Raja <msathis.akl@hotmail.com>
2024-11-27 19:00:24 +0800

    authenticated using session

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import routes from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import session from "express-session";
+import { mockUsers } from "./utils/constants.js";
 const app = express();
 
 app.use(express.json());
@@ -30,4 +31,42 @@ app.get("/", (req, res) => {
   req.session.visited = true;
   res.cookie("hello", "Express", { maxAge: 10000 });
   res.status(201).send({ msg: "Hello Express" });
+});
+
+app.post("/api/auth", (req, res) => {
+  const {
+    body: { username, password },
+  } = req;
+  const findUser = mockUsers.find((user) => user.username === username);
+  if (!findUser || findUser.password !== password)
+    return res.status(401).send({ msg: "Bad Credential" });
+  req.session.user = findUser;
+  return res.status(200).send(findUser);
+});
+
+app.get("/api/auth/status", (req, res) => {
+  req.sessionStore.get(req.sessionID, (err, session) => {
+    console.log(session);
+  });
+  return req.session.user
+    ? res.status(200).send(req.session.user)
+    : res.status(401).send({ msg: "Not authenticated" });
+});
+
+app.post("/api/cart", (req, res) => {
+  if (!req.session.user) return res.sendStatus(401);
+  const { body: item } = req;
+
+  const { cart } = req.session;
+  if (cart) {
+    cart.push(item);
+  } else {
+    req.session.cart = [item];
+  }
+  return res.status(201).send(item);
+});
+
+app.get("/api/cart", (req, res) => {
+  if (!req.session.user) return res.sendStatus(401);
+  return res.send(req.session.cart ?? []);
 });
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,9 @@
 export const mockUsers = [
-  { id: 1, username: "sathish", displayName: "Sathish" },
-  { id: 2, username: "raja", displayName: "Raja" },
-  { id: 3, username: "david", displayName: "David" },
-  { id: 4, username: "manoj", displayName: "Manoj" },
-  { id: 5, username: "karan", displayName: "Karan" },
-  { id: 6, username: "nila", displayName: "Nila" },
-  { id: 7, username: "ravi", displayName: "Ravi" },
+  { id: 1, username: "sathish", displayName: "Sathish", password: "test123" },
+  { id: 2, username: "raja", displayName: "Raja", password: "test123" },
+  { id: 3, username: "david", displayName: "David", password: "test123" },
+  { id: 4, username: "manoj", displayName: "Manoj", password: "test123" },
+  { id: 5, username: "karan", displayName: "Karan", password: "test123" },
+  { id: 6, username: "nila", displayName: "Nila", password: "test123" },
+  { id: 7, username: "ravi", displayName: "Ravi", password: "test123" },
 ];
